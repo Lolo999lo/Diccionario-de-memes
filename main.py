56dfa227b98dafcd537d3345a24fb53812accbab
Michelle <165445991+Lolo999lo@users.noreply.github.com>
2024-05-09 17:21:43 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+meme_dict = {
+            "CRINGE" : "Algo excepcionalmente raro o embarazoso",
+            "LOL" : "Una respuesta común a algo gracioso",
+            "BRO" : "Así se le dice a un amigo",
+            "CREEPY" : "Algo aterrador o siniestro"
+            "HATER" : "Alguien odioso o que da puro hate (malas opiniones)"
+            }
+            
+word = input("Escribe una palabra que no entiendas o no conoscas (Que sea en mayuscula por favor)")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
